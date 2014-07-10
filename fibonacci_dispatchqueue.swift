cf819c3cbba71e3e9fea6021fe2bef9f75c08fb8
Brandon Evans <brandon@brandonevans.ca>
2014-07-09 21:29:49 -0600

    Add Fibonacci example with dispatch queues

---
+++ fibonacci_dispatchqueue.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+func fibonacci(index: UInt) -> UInt {
+    switch (index) {
+	    case 0, 1:
+	        return index
+	    default:
+	        return fibonacci(index - 1) + fibonacci(index - 2)
+    }
+}
+
+println("--- Concurrent Queue ---")
+let concurrentQueue = dispatch_queue_create("ca.brandonevans.GCDQueueDemo.Concurrent", DISPATCH_QUEUE_CONCURRENT);
+dispatch_apply(10, concurrentQueue, { fibNum in
+	println("fibonacci number \(fibNum): \(fibonacci(fibNum))")
+})
+
+println("\n--- Serial Queue ---")
+let serialQueue = dispatch_queue_create("ca.brandonevans.GCDQueueDemo.Serial", DISPATCH_QUEUE_SERIAL);
+dispatch_apply(10, serialQueue, { fibNum in
+    println("fibonacci number \(fibNum): \(fibonacci(fibNum))")
+})
